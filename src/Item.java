39f22d55a9a67f813a827315760b965ed44d9c9d
anderson92zolis <azolis92@hotmail.es>
2023-03-21 09:59:51 +0100

    first commit

---
+++ src/Item.java
@@ -0,0 +1,15 @@
+public class Item {
+     private String name;
+     private String type;
+     private double price;
+     private int wearPorcentage;
+
+    public Item(String name, String type, double price,int wearPorcentage) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+
+
+}
---
+++ src/Seller.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Seller {
+    private String name;
+    private String type;
+    private String city;
+    private List<Item> inventory;
+
+
+
+}
